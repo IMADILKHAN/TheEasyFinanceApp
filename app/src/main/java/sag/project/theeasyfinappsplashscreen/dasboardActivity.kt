08ce9c62babd06264980a0086b9e5980096a56ca
Shudhanshu <saggreener@gmail.com>
2023-11-22 23:12:24 +0530

    Designed Dashboard and added fragments to the navigation bar i.e. HomeFragment, CardFragment, InvestFragment and ProfileFragment

--- app/src/main/java/sag/project/theeasyfinappsplashscreen/dasboardActivity.kt
+++ app/src/main/java/sag/project/theeasyfinappsplashscreen/dasboardActivity.kt
@@ -2,10 +2,39 @@ package sag.project.theeasyfinappsplashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class dasboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dasboard)
+
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottomNavViewDashboard)
+
+        replaceWithFragment(HomeFragment())
+        //Setting Home Fragment visible by default
+
+        bottomView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.HomeItem -> replaceWithFragment(HomeFragment())
+                R.id.CardsItem -> replaceWithFragment(CardFragment())
+                R.id.InvestItem -> replaceWithFragment(InvestFragment())
+                R.id.UsersItem -> replaceWithFragment(ProfileFragment())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceWithFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flDashboard,fragment)
+        fragmentTransaction.commit()
     }
 }
